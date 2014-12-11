2725f89a86fa611434162d388964be35b3442601
Apple <apple@AppledeiMac.local>
2014-12-11 11:26:41 +0800

    Initial Commit

---
+++ BeeCool_Client_IOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BeeCool_Client_IOS
+//
+//  Created by Apple on 14/12/11.
+//  Copyright (c) 2014年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BeeCool_Client_IOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BeeCool_Client_IOS
+//
+//  Created by Apple on 14/12/11.
+//  Copyright (c) 2014年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
