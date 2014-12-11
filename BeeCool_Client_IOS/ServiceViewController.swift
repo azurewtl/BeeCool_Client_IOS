3017a7309f1b7e4d0c970dd07595d5778a34d48d
caiyang <apple@AppledeiMac-2.local>
2014-12-11 12:00:24 +0800

    九宫格

--- BeeCool_Client_IOS/ServiceViewController.swift
+++ BeeCool_Client_IOS/ServiceViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ServiceViewController: UIViewController {
+class ServiceViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
 
+    @IBOutlet var serverCollectionview: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("serviceCell", forIndexPath: indexPath) as UICollectionViewCell
+        cell.layer.borderWidth = 1;
+        cell.layer.borderColor = UIColor.grayColor().CGColor
+        return cell
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize{
+        var width:CGFloat = 1 / 3
+        width = width * self.view.frame.width
+    return CGSizeMake(width, width)
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets{
+        return UIEdgeInsetsMake(120, 0, 0, 0)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+   
 
 }
 
